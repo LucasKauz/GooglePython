ab13702f600aa20a752c069c81f28549f4bed5d4
Lucas M. R <lucaskauz@gmail.com>
2013-01-16 22:35:40 -0200

    Alterando sys_arv e adicionando comentários em .txt

--- sys_argv.py
+++ sys_argv.py
@@ -5,7 +5,7 @@ import sys
 
 # Gather our code in a main() function
 def main():
-    print 'Hello there', repeat(sys.argv[1],1)
+    print 'Hello there', repeat(sys.argv[1],True)
     # Command line args are in sys.argv[1], sys.argv[2] ...
     # sys.argv[0] is the script name itself and can be ignored
 
