ecbfdeea9c42ad87aa7ee4ae54b9b534f08572b3
Lucas M. R <lucaskauz@gmail.com>
2013-01-16 22:35:40 -0200

    Adicionando definição de funções DEF

--- sys_argv.py
+++ sys_argv.py
@@ -5,11 +5,23 @@ import sys
 
 # Gather our code in a main() function
 def main():
-    print 'Hello there', sys.argv[1]
+    print 'Hello there', repeat(sys.argv[1],1)
     # Command line args are in sys.argv[1], sys.argv[2] ...
     # sys.argv[0] is the script name itself and can be ignored
 
 # Standard boilerplate to call the main() function to begin
 # the program.
+
+# Defines a "repeat" function that takes 2 arguments.
+def repeat(s, exclaim):
+    """Returns the string s repeated 3 times.
+    If exclaim is true, add exclamation marks.
+    """
+
+    result = s +', '+ s +', '+ s # can also use "s * 3" which is faster (Why?)
+    if exclaim:
+        result = result + '!!!'
+    return result
+
 if __name__ == '__main__':
     main()
