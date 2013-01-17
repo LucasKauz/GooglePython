280a863069b2980cbd577b52f132f15519a74d5e
Lucas M. R <lucaskauz@gmail.com>
2013-01-16 22:35:40 -0200

    Adicionado material sobre string

--- sys_argv.py
+++ sys_argv.py
@@ -6,6 +6,20 @@ import sys
 # Gather our code in a main() function
 def main():
     print 'Hello there', repeat(sys.argv[1],True)
+    
+    # Convers\xc3o de tipos
+    pi = 3.14
+    ##text = 'The value of pi is ' + pi      ## NO, does not work
+    text = 'The value of pi is '  + str(pi)  ## yes
+    print text
+
+
+    raw = r'this\t\n and that' # O r a frenta de string indica para o python interpreta-la literalmente
+    print raw     ## this\t\n and that
+
+    multi = """It was the best of times.
+    It was the worst of times."""
+
     # Command line args are in sys.argv[1], sys.argv[2] ...
     # sys.argv[0] is the script name itself and can be ignored
 
